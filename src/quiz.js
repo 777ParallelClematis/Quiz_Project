855db4db2d58646a9d17a95c5813c9b4c954d48a
777ParallelClematis <erin-colane@live.nmit.ac.nz>
2024-03-14 12:45:00 +1300

    first part of quiz. more to add in teams.

---
+++ src/quiz.js
@@ -0,0 +1,16 @@
+// Install Node.js
+
+// Install Readline - later
+
+//Questions
+
+const question = [
+    {
+        question : "what is 2 + 2?",
+        options : ["2","3","4","5"],
+        answer: "4",
+    },
+    {   question: "What is the capital of New Zealand?",
+          answer: ["Auckland,", "Wellington", "Nelson", "Christchurch",]
+    }
+]
